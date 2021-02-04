5f6543db0d11b2db6385b193e4d854de75e2182a
Mohammad Hossain <moh.hossain98@gmail.com>
2021-02-03 21:46:52 -0500

    changing api link

--- src/redux/player/player.actions.js
+++ src/redux/player/player.actions.js
@@ -1,39 +1,18 @@
 import axios from 'axios';
-<<<<<<< HEAD
-import PlayerActionTypes from './player.types';
-
-//ACTION CREATORS
-export const fetchAllPlayers = (payload) => ({
-  type: PlayerActionTypes.FETCH_ALL_PLAYERS,
-=======
 import ItemActionType from './player.types';
 
 //ACTION CREATORS
 export const fetchAllItems = (payload) => ({
   type: ItemActionType.FETCH_ALL_ITEMS,
->>>>>>> fff8f7ee074f8573a0bd46f3574967d2f9653852
   payload,
 });
 
 //THUNKS
 
-<<<<<<< HEAD
-export const fetchAllPlayersThunk = () => {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.get('http://localhost:8080/api/players');
-      console.log('data', data);
-      dispatch(fetchAllPlayers(data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-};
-=======
 export const fetchAllItemsThunk = (user) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get('http://localhost:8080/api/item' + user);
+      const { data } = await axios.get('https://capstone-recycle.herokuapp.com/api/item/' + user);
       console.log('data', data);
       dispatch(fetchAllItems(data));
     } catch (error) {
@@ -41,4 +20,3 @@ export const fetchAllItemsThunk = (user) => {
     }
   };
 };
->>>>>>> fff8f7ee074f8573a0bd46f3574967d2f9653852
--- src/redux/player/player.reducer.js
+++ src/redux/player/player.reducer.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import PlayerActionTypes from './player.types';
-
-const INITIAL_STATE = {
-  allPlayers: [],
-};
-
-const playerReducer = (state = INITIAL_STATE, action) => {
-  console.log('PL', action);
-  switch (action.type) {
-    case PlayerActionTypes.FETCH_ALL_PLAYERS:
-      return { ...state, allPlayers: action.payload };
-=======
 import ItemActionType from './player.types';
 
 const INITIAL_STATE = {
@@ -22,14 +9,9 @@ const itemReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ItemActionType.FETCH_ALL_ITEMS:
       return { ...state, items: action.payload };
->>>>>>> fff8f7ee074f8573a0bd46f3574967d2f9653852
     default:
       return state;
   }
 };
 
-<<<<<<< HEAD
-export default playerReducer;
-=======
 export default itemReducer;
->>>>>>> fff8f7ee074f8573a0bd46f3574967d2f9653852
