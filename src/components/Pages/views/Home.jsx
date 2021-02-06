fdb7d0fb231ee4778658a26d89587089136c2775
Mohammad Hossain <moh.hossain98@gmail.com>
2021-02-06 15:02:49 -0500

    adding points

--- src/components/Pages/views/Home.jsx
+++ src/components/Pages/views/Home.jsx
@@ -17,6 +17,12 @@ class Home extends React.Component {
   render(){
     const items = this.props.items;
     console.log(items)
+    const total = 0;
+    for(let i = 0; i < items.length; i++){
+      total = total + items[i].points;
+    }
+
+    console.log('Points: ', total)
     return <div>
     <AfterLogin />
     <div className = "recycled">
